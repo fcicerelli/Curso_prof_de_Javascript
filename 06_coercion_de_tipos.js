b874aa8d017fa4679b2c47412c20654cf525d462
Fabio Cicerelli <fcicerellitic@gmail.com>
2026-06-27 12:12:10 -0300

    coercion de tipos y booleanos

---
+++ 06_coercion_de_tipos.js
@@ -0,0 +1,8 @@
+console.log(10 + "5"); // concatenación, resultado: "105"
+console.log(10 - "5"); // resta, resultado: 5
+
+console.log(Number("10") + 10); // convierte el string a number, resultado: 20
+
+
+console.log("10" == 10);  // true, convierte el string a number
+console.log("10" === 10); // false, no convierte el string a number
---
+++ 07_booleanos.js
@@ -0,0 +1,12 @@
+// Booleano true / false
+
+// Ciclos / Condicionales
+
+let notificaciones_activas = false;
+let isActive = false;
+
+let resultado = Boolean(1);  // true
+let resultado2 = Boolean(0);  // false
+
+// evaluan falso:
+// undefined, Null, 0, NaN, "", false
